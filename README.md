39db4c9a51b93cf514bb0a6f97fd06925b1e047e
Jorge Alejandro Baeza San Martin <jorge.baeza1501@alumnos.ubiobio.cl>
2020-01-04 14:13:35 -0300

    Primer commit - rest server

---
+++ README.md
@@ -0,0 +1,17 @@
+# Socket-Server
+
+Reconstruir módulos de node
+```
+npm install
+```
+
+Generar el DIST
+```
+tsc -w
+```
+
+Levantar Servidor, cualquier de esos comandos
+```
+nodemon dist/
+node dist/
+```
---
+++ index.ts
@@ -0,0 +1,21 @@
+import { SERVER_PORT } from "./global/environment";
+import Server from "./classes/server";
+import { router } from './routes/router'
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const server = new Server();
+
+//BodyParser. Lo que me posteen lo tomo y genero un objeto JS
+server.app.use( bodyParser.urlencoded({ extended: true }) );
+server.app.use( bodyParser.json() );
+
+//CORS. Permitiendo que cualquier persona pueda llamar mis servicios
+server.app.use( cors({ origin: true, credentials: true }) )
+
+
+server.app.use('/', router );
+
+server.start( () => {
+    console.log(`Servidor corriendo en el puerto ${ SERVER_PORT }`);
+});
